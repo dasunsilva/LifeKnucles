cad7dbef45c719c68f8ea9f7b34b1bd91bebb561
Dasun Theekshana <e19443@eng.pdn.ac.lk>
2024-09-03 22:17:32 +0530

    Revert "Login page"

--- components/GetStartedAuth.tsx
+++ components/GetStartedAuth.tsx
@@ -1,53 +1,71 @@
-import { View, Text } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 import { Button, Divider } from "react-native-paper";
-import CustomButton from "./CustomButton";
-import { useRouter } from "expo-router";
+import { Link } from "expo-router";
 
 export default function GetStartedAuth() {
-  const router = useRouter();
   return (
     <View className="mt-6">
-      <View className="flex-row justify-between row space-x-4">
-        <CustomButton
-          text={"Sign-in"}
-          onpress={() => router.push("Login")}
-          icon="login"
-        />
+      <View className="flex-row space-x-4">
+        <TouchableOpacity className="w-[150px] h-[55px]">
+          <Button
+            mode="contained"
+            className="w-full h-full justify-center"
+            contentStyle={{ height: "100%" }}
+            buttonColor="#419A00"
+            textColor="white"
+            labelStyle={{ fontSize: 16 }}
+            onPress={() => console.log("Sign-in Button Pressed")}
+          >
+            <Link href="/home" className="font-pbold">
+              Sign-in
+            </Link>
+          </Button>
+        </TouchableOpacity>
 
-        <CustomButton
-          text={"Sign-up"}
-          onpress={() => console.log("Sign Up")}
-          icon="account-plus"
-        />
+        <TouchableOpacity className="w-[150px] h-[55px]">
+          <Button
+            mode="contained"
+            className="w-full h-full justify-center"
+            contentStyle={{ height: "100%" }}
+            buttonColor="#419A00"
+            textColor="white"
+            labelStyle={{ fontSize: 16 }}
+            onPress={() => console.log("Sign-up Button Pressed")}
+          >
+            <Text className="font-pbold">Sign-up</Text>
+          </Button>
+        </TouchableOpacity>
       </View>
-
       <Text className="text-white text-center mt-4">or</Text>
       <Divider className="h-0.5 bg-white m-2" />
-
       <View className="flex-row space-x-4 justify-center mt-5">
-        <Button
-          mode="contained-tonal"
-          icon="google"
-          className="w-[140px] h-[55px]"
-          contentStyle={{ height: "100%" }}
-          labelStyle={{ fontSize: 17 }}
-          onPress={() => console.log("Google Sign-in Button Pressed")}
-          buttonColor="#2E8B57"
-        >
-          Google
-        </Button>
+        <TouchableOpacity className="w-[140px] h-[55px]">
+          <Button
+            mode="contained-tonal"
+            icon="google"
+            className="w-full h-full justify-center items-center"
+            contentStyle={{ height: "100%" }}
+            labelStyle={{ fontSize: 17 }}
+            onPress={() => console.log("Google Sign-in Button Pressed")}
+            buttonColor="#2E8B57"
+          >
+            Google
+          </Button>
+        </TouchableOpacity>
 
-        <Button
-          mode="contained-tonal"
-          icon="facebook"
-          className="w-[140px] h-[55px]"
-          contentStyle={{ height: "100%" }}
-          labelStyle={{ fontSize: 17 }}
-          onPress={() => console.log("Facebook Sign-in Button Pressed")}
-          buttonColor="#1877F2"
-        >
-          Facebook
-        </Button>
+        <TouchableOpacity className="w-[140px] h-[55px]">
+          <Button
+            mode="contained-tonal"
+            icon="facebook"
+            className="w-full h-full justify-center items-center"
+            contentStyle={{ height: "100%" }}
+            labelStyle={{ fontSize: 17 }}
+            onPress={() => console.log("Facebook Sign-in Button Pressed")}
+            buttonColor="#1877F2"
+          >
+            FaceBook
+          </Button>
+        </TouchableOpacity>
       </View>
     </View>
   );
